bab15f8eeb56891b6a7961d5a4a0f685afc9ee5e
Callum Hands <chands@pceftpos.com.au>
2019-06-11 15:20:42 +1000

    i test things

--- TestingGround/TestingGround/TestingGround.cpp
+++ TestingGround/TestingGround/TestingGround.cpp
@@ -4,18 +4,56 @@
 #include "pch.h"
 #include <iostream>
 
+__interface IAction {
+public:
+
+	void actionVoid();
+};
+
+class MainAction : public IAction {
+public:
+	MainAction() {}
+	virtual ~MainAction() {}
+	void actionVoid() override;
+};
+
+class SecAction : public IAction {
+public:
+	SecAction() {}
+	virtual ~SecAction() {}
+	void actionVoid() override;
+};
+
+class MainActionClass {
+public:
+	MainActionClass() { }
+	virtual ~MainActionClass() {}
+
+	void DoAction(IAction &action);
+private:
+};
+
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	MainAction m;
+	MainActionClass mC;
+	SecAction s;
+	mC.DoAction(m);
+	mC.DoAction(s);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void MainActionClass::DoAction(IAction &action)
+{
+	action.actionVoid();
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void MainAction::actionVoid()
+{
+	std::cout << "Hello World!\n";
+}
+
+void SecAction::actionVoid()
+{
+	std::cout << "This is an action test\n";
+}
